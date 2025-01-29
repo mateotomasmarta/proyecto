f7644ed8feed32a840329678ce0b8d0bfd7f89a7
Mateo Marta <martamateotomas@gmail.com>
2025-01-29 15:06:14 -0300

    reverti el archivo hellogit.py

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 print ("hello GIT!, este es mi primer archivo, y estoy muy emocionado de aprender GIT")
+=======
+print ("hello GIT!")
+>>>>>>> parent of 96c6293 (actualice el archivo hellogit)
 
